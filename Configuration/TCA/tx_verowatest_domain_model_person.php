6a9f9c2199d64dca781e488d08a7b94b88ec52fc
checkitsedo <56700394+checkitsedo@users.noreply.github.com>
2024-02-13 13:29:36 +0100

    Create tx_verowatest_domain_model_person.php

---
+++ Configuration/TCA/tx_verowatest_domain_model_person.php
@@ -0,0 +1,187 @@
+<?php
+return [
+	'ctrl' => [
+		'title' => 'LLL:EXT:verowa_test/Resources/Private/Language/locallang_db.xlf:tx_verowatest_domain_model_person',
+		'label' => 'person_id',
+		'tstamp' => 'tstamp',
+		'crdate' => 'crdate',
+		'cruser_id' => 'cruser_id',
+		'sortby' => 'sorting',
+		'versioningWS' => true,
+		'languageField' => 'sys_language_uid',
+		'transOrigPointerField' => 'l10n_parent',
+		'transOrigDiffSourceField' => 'l10n_diffsource',
+		'delete' => 'deleted',
+		'enablecolumns' => [
+			'disabled' => 'hidden',
+			'starttime' => 'starttime',
+			'endtime' => 'endtime',
+		],
+		'searchFields' => 'person_id,name,firstname,lastname',
+		'iconfile' => 'EXT:verowa_test/Resources/Public/Icons/tx_verowatest_domain_model_person.gif'
+	],
+	'external' => [
+		'general' => [
+			0 => [
+				'connector' => 'json',
+				'parameters' => [
+					'uri' => 'https://api.verowa.ch/geteventdetails/stjosef-zuerich/1ad89c27e1a89ef6aa34f34e4adf7448/0',
+					'encoding' => 'utf-8',
+					'headers' => [
+						'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:75.0) Gecko/20100101 Firefox/75.0',
+						'Accept' => 'application/json'
+					],
+				],
+				'data' => 'array',
+				'arrayPath' => 'organizer',
+				'referenceUid' => 'person_id',
+				'group' => 'stjosef',
+				'priority' => 10,
+				'description' => 'Import of all events from Verowa'
+			],
+		],
+	],
+	'types' => [
+		'1' => ['showitem' => 'sys_language_uid, l10n_parent, l10n_diffsource, hidden, person_id, name, firstname, lastname, --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.access, starttime, endtime'],
+	],
+	'columns' => [
+		'sys_language_uid' => [
+			'exclude' => true,
+			'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.language',
+			'config' => [
+				'type' => 'select',
+				'renderType' => 'selectSingle',
+				'special' => 'languages',
+				'items' => [
+					[
+						'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.allLanguages',
+						-1,
+						'flags-multiple'
+					],
+				],
+				'default' => 0,
+			],
+		],
+		'l10n_parent' => [
+			'displayCond' => 'FIELD:sys_language_uid:>:0',
+			'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.l18n_parent',
+			'config' => [
+				'type' => 'select',
+				'renderType' => 'selectSingle',
+				'default' => 0,
+				'items' => [
+					['', 0],
+				],
+				'foreign_table' => 'tx_verowatest_domain_model_person',
+				'foreign_table_where' => 'AND {#tx_verowatest_domain_model_person}.{#pid}=###CURRENT_PID### AND {#tx_verowatest_domain_model_person}.{#sys_language_uid} IN (-1,0)',
+			],
+		],
+		'l10n_diffsource' => [
+			'config' => [
+				'type' => 'passthrough',
+			],
+		],
+		'hidden' => [
+			'exclude' => true,
+			'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.visible',
+			'config' => [
+				'type' => 'check',
+				'renderType' => 'checkboxToggle',
+				'items' => [
+					[
+						0 => '',
+						1 => '',
+						'invertStateDisplay' => true
+					],
+				],
+			],
+		],
+		'starttime' => [
+			'exclude' => true,
+			'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.starttime',
+			'config' => [
+				'type' => 'input',
+				'renderType' => 'inputDateTime',
+				'eval' => 'datetime,int',
+				'default' => 0,
+				'behaviour' => [
+					'allowLanguageSynchronization' => true
+				],
+			],
+		],
+		'endtime' => [
+			'exclude' => true,
+			'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.endtime',
+			'config' => [
+				'type' => 'input',
+				'renderType' => 'inputDateTime',
+				'eval' => 'datetime,int',
+				'default' => 0,
+				'range' => [
+					'upper' => mktime(0, 0, 0, 1, 1, 2038)
+				],
+				'behaviour' => [
+					'allowLanguageSynchronization' => true
+				],
+			],
+		],
+
+		'person_id' => [
+			'exclude' => true,
+			'label' => 'Person Id',
+			'config' => [
+				'type' => 'input',
+			],
+			'external' => [
+				0 => [
+					'field' => 'person_id'
+				],
+			],
+		],
+		'name' => [
+			'exclude' => true,
+			'label' => 'Name',
+			'config' => [
+				'type' => 'input',
+				'size' => 50,
+				'max' => 255,
+				'eval' => 'trim',
+			],
+			'external' => [
+				0 => [
+					'field' => 'name'
+				],
+			],
+		],
+    'firstname' => [
+			'exclude' => true,
+			'label' => 'First Name',
+			'config' => [
+				'type' => 'input',
+				'size' => 50,
+				'max' => 255,
+				'eval' => 'trim',
+			],
+			'external' => [
+				0 => [
+					'field' => 'firstname'
+				],
+			],
+		],
+    'lastname' => [
+			'exclude' => true,
+			'label' => 'Lastname',
+			'config' => [
+				'type' => 'input',
+				'size' => 50,
+				'max' => 255,
+				'eval' => 'trim',
+			],
+			'external' => [
+				0 => [
+					'field' => 'lastname'
+				],
+			],
+		],
+  ],
+];
